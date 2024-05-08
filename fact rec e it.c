4d5e3f6149093fb490d505ebd23869bffb344d5b
Flavio Cortes <166086611+flaviocortesaraya@users.noreply.github.com>
2024-05-08 10:36:06 -0400

    Update fact rec e it.c

--- fact rec e it.c
+++ fact rec e it.c
@@ -1,1 +1,28 @@
 #include<stdio.h>
+#include<stdio.h>
+
+int factorial(int);
+
+void main(){
+    int n;
+    printf("ingresar el numero ");
+    scanf("%D",&n);
+    printf("El factorial del numero %d es : %d ",n,factorial(n));
+}
+
+int factorial(int n){
+    int calculo
+    if(n<0){
+        return 0;
+    }
+    if(n==0||n==1){
+        return 1;
+    }else{
+        calculo =n*factorial(n-1);
+        if(calculo<=0){
+            return 0;
+        }
+        return calculo;
+    }
+
+}
